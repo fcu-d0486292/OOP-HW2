86fc85399ae1c8ba25cd07958e6590757c549404
fcu-d0486292 <d0486292@fcu.edu.tw>
2016-10-26 17:29:39 +0800

    HW3

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -4,12 +4,21 @@ public class StringComparator {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		String x,y;
-		System.out.print("Enter a string 1: ");
-		x=input.next();
-		System.out.print("Enter a string 2: ");
-		y=input.next();
-		if(x.compareToIgnoreCase(y)==0)System.out.println("The two strings are the same.");
-		else System.out.println("The two strings are not the same.");
+		while(true){
+			System.out.print("Enter a string 1: ");
+			x=input.next();
+			System.out.print("Enter a string 2: ");
+			y=input.next();
+			if(x.compareToIgnoreCase(y)==0){
+				System.out.println("The two strings are the same.");
+				break;
+			}
+			else {
+				System.out.println("The two strings are not the same.");
+			}
+		}
+		
+		
 		input.close();
 	}
 }
